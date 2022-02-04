1f47d29385719646865dfe8ba1dcaf79ec566207
Tarun Murugan <tarun.murugan24@gmail.com>
2022-02-04 14:54:04 -0800

    starter + lib

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,5 @@
 public class SkillDemo {
-
+    public static String exclaim(String s) {
+        return s + "!";
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void exclaimTest() {
+        assertEquals("hello", SkillDemo.exclaim("hello"));
+    }
+}
